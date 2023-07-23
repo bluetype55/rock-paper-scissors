35207feaecd2c253110c43c8b3062dcd7db664e3
bluetype55 <118740628+bluetype55@users.noreply.github.com>
2023-07-24 07:26:34 +0900

    widget extraction

--- lib/main.dart
+++ lib/main.dart
@@ -2,29 +2,55 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('가위 바위 보'),
-          centerTitle: true,
+    const MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Rock, Paper, Scissors',
+      theme: ThemeData(
+        primarySwatch: Colors.brown,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('가위 바위 보'),
+        centerTitle: true,
+      ),
+      body: Container(
+        width: double.infinity,
+        height: 400,
+        decoration: BoxDecoration(
+          border: Border.all(),
         ),
-        body: Container(
-          width: double.infinity,
-          height: 400,
-          decoration: BoxDecoration(
-            border: Border.all(),
-          ),
-          child: Center(
-            child: Container(
-              width: 100,
-              height: 50,
-              decoration: BoxDecoration(
-                border: Border.all(),
-              ),
+        child: Center(
+          child: Container(
+            width: 100,
+            height: 50,
+            decoration: BoxDecoration(
+              border: Border.all(),
             ),
           ),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
