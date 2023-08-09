604e37566f5067e73e86e988e8816f2594d16e5a
bluetype55 <118740628+bluetype55@users.noreply.github.com>
2023-08-09 19:06:56 +0900

    complete

--- lib/game/widget/game_result.dart
+++ lib/game/widget/game_result.dart
@@ -1,14 +1,45 @@
+import 'package:dart_rps_game/game/enum.dart';
 import 'package:flutter/material.dart';
 
 class GameResult extends StatelessWidget {
   final bool isDone;
+  final Result? result;
+  final VoidCallback callback;
 
-  const GameResult({required this.isDone, super.key});
+  const GameResult(
+      {required this.isDone, required this.callback, this.result, super.key});
 
   @override
   Widget build(BuildContext context) {
     if (isDone) {
-      return const Placeholder();
+      return Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            result!.displayString,
+            style: const TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(
+            height: 8,
+          ),
+          TextButton(
+            onPressed: () => callback.call(),
+            child: const Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(Icons.refresh),
+                SizedBox(
+                  width: 5,
+                ),
+                Text(
+                  '다시하기',
+                  style: TextStyle(fontSize: 24),
+                ),
+              ],
+            ),
+          ),
+        ],
+      );
     }
     return const Center(
       child: Text(
